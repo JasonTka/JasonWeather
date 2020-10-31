c9a834320f4a65abb2f00b4702dc87605190d620
Jason <52074080@qq.com>
2020-10-31 13:52:02 +0800

    新增切换城市和手动更新天气信息功能

--- app/src/main/java/com/example/jasonweather/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/example/jasonweather/ui/place/PlaceFragment.kt
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.jasonweather.MainActivity
 import com.example.jasonweather.R
 import com.example.jasonweather.ui.weather.WeatherActivity
 import kotlinx.android.synthetic.main.fragment_place.*
@@ -32,7 +33,7 @@ class PlaceFragment : Fragment() {
     @SuppressLint("FragmentLiveDataObserve")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        if (viewModel.isPlaceSaved()) {
+        if (activity is MainActivity && viewModel.isPlaceSaved()) {
             val place = viewModel.getSavedPlace()
             val intent = Intent(context, WeatherActivity::class.java).apply {
                 putExtra("location_lng", place.location.lng)
