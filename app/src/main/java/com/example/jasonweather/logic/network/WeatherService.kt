e9e7a9d8811c8c735dd34b58587d889ffdd5c579
Jason <52074080@qq.com>
2020-10-31 11:56:56 +0800

    查询当天天气及未来天气

---
+++ app/src/main/java/com/example/jasonweather/logic/network/WeatherService.kt
@@ -0,0 +1,24 @@
+package com.example.jasonweather.logic.network
+
+import com.example.jasonweather.JasonWeatherApplication
+import com.example.jasonweather.logic.model.DailyResponse
+import com.example.jasonweather.logic.model.RealtimeResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface WeatherService {
+
+    @GET("v2.5/${JasonWeatherApplication.TOKEN}/{lng},{lat}/realtime.json")
+    fun getRealtimeWeather(
+        @Path("lng") lng: String,
+        @Path("lat") lat: String
+    ): Call<RealtimeResponse>
+
+    @GET("v2.5/${JasonWeatherApplication.TOKEN}/{lng},{lat}/daily.json")
+    fun getDailyWeather(
+        @Path("lng") lng: String,
+        @Path("lat") lat: String
+    ): Call<DailyResponse>
+
+}
